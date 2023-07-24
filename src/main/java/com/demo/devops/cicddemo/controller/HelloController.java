d1d01efa317633dbac00cebdcc41f3ce568e7b7f
dmnglore <35909263+dmnglor@users.noreply.github.com>
2023-07-24 12:29:50 +0530

    firstcommit

---
+++ src/main/java/com/demo/devops/cicddemo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.demo.devops.cicddemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello to the world of devops ";
+	}
+
+}
